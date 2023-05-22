bb8a6550f747494735c28d2cd4a17b077a0fd7ad
cjd1011 <77751497+cjd1011@users.noreply.github.com>
2023-05-21 23:47:31 -0500

    Add files via upload

--- excel.py
+++ excel.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+import pip
+pip.main(["install", "openpyxl"])
+
 st.title('Aprende a subir tu base de datos de Excel a la Web')
 
 df = pd.read_excel('USD-COP.xlsx')
