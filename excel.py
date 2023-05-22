8c398af95d66a61c5f335c417686c226c536c9e9
cjd1011 <77751497+cjd1011@users.noreply.github.com>
2023-05-21 23:30:33 -0500

    Add files via upload

--- excel.py
+++ excel.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 
 st.title('Aprende a subir tu archivo de excel a la Web')
+
+
+df = pd.read_excel('USD-COP.xlsx')
+
+st.write(df)
+                   
+                   
